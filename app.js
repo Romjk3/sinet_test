1170046b95b992f3e61d8e5fb3ea6ec2d2d6845c
Prokopev Roman <romjke1996@mail.ru>
2018-08-09 17:37:11 +0900

    Recursive

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 'use strict';
 
 const FS = require('fs');
-const COMMANDER = require('commander');
+const program = require('commander');
 const PATH = require('path');
 
-var folder = "./";
-
-
-if (process.argv[2] != undefined) {
-    folder = process.argv[2];
-}
-
-function safeReadDirSync (path) {
+function safeReadDirSync (path, depth, option) {
     let dirData = [];
     let directories = [];
     let files = [];
@@ -25,30 +18,30 @@ function safeReadDirSync (path) {
         else throw ex;
     }
 
-    dirData.forEach((file, index) => {
+    dirData.forEach(file => {
         let stats;
         try {stats = FS.statSync(path + '/' +file); }
         catch (e) { return null; }
 
         if (stats.isDirectory()) {
-            directories.push(safeReadDirSync(path + '/' +file));
+            directories.push(path + '/' +file);
         }
         else if (stats.isFile()) {
             files.push(file);
         }
     });
     path = PATH.basename(path);
-    return {path , directories, files};
+    viewDir({path , directories, files}, depth, option);
 }
 
 function viewDir(item, depth, option){
     item['directories'].forEach((element, index, arr) => {
         if ((index + 1 == arr.length) && (item['files'].length == 0 || option == '-d')){
-            console.log(depth + '└── ' + element['path']);
-            viewDir(element, depth + '   ', option);
+            console.log(depth + '└── ' + PATH.basename(element));
+            safeReadDirSync(element, depth + '   ', option);
         } else {
-            console.log(depth + '├── ' + element['path']);
-            viewDir(element, depth + '│   ', option);
+            console.log(depth + '├── ' + PATH.basename(element));
+            safeReadDirSync(element, depth + '│   ', option);
         }
     });
     if (option != '-d') {
@@ -62,4 +55,29 @@ function viewDir(item, depth, option){
     }
 }
 
-viewDir(safeReadDirSync(folder), '', '-d');
+program
+    .version('0.0.1')
+    .description('sinet_test tree');
+
+program
+    .command('tree [path]')
+    .description('Show tree')
+    .option('-d, --directories','Only directories')
+    .action((path, options) => {
+        if (path === undefined) path = '.';
+        console.log(path);
+        FS.stat(path, (err, stat) => {
+            let mode = options.directories ? '-d':'';
+            if ((err == null) &&  stat.isDirectory()){
+                safeReadDirSync(path , '', mode);
+            } else if (stat.isFile()){
+                console.log('Вы указали файл');
+            } else if(err.code == 'ENOENT') {
+                console.log('Такого каталога не существует', err.code);
+            } else {
+                console.log('Some other error: ', err.code);
+            }
+        });
+    });
+
+program.parse(process.argv);
